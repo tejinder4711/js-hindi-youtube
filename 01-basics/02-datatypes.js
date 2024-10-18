87003b6cc48f7ef979e273f8501ea09df420addc
tejinder4711 <86477696+tejinder4711@users.noreply.github.com>
2024-10-18 05:01:08 +0000

    datatype

--- 01-basics/02-datatypes.js
+++ 01-basics/02-datatypes.js
@@ -1,4 +1,22 @@
 "use strict";  //treat all JS code as newer version
 
 // alert("hello dost")
-console.log(125 + 135);
+// console.log(125 + 135); // we are using nodejs, not browser
+// code readability should be higher
+
+let name = "Tejinder" // string
+let age = 23    // number 
+let isLoggedIn = false // boolean
+
+// number => 2 to power 53
+// bigint
+// string = we can use in single '' or "" quotes
+//boolean => true / false
+// null => standalone value (empty)
+// undefined => value is not defined
+// symbol => used for unique-ness
+// 
+console.log(typeof "tejinder")
+
+
+
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # js-hindi-youtube
+
 js-hindi-chai-with-code-youtube
+
+// tc 39 / ecma script
+
+// mdn documentation
